84a5618d2de35fa00b74c94bd021b52885ec746d
Anas <anas.bettouzia@esprit.tn>
2025-07-17 17:10:46 +0100

    login-push

---
+++ Project/src/main/java/com/example/Project/role/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.Project.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+    // Additional query methods can be defined here if needed
+}
